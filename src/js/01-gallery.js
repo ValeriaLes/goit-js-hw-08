8f7f0f8a3933805cd41900e7172036910635ddcb
ValeriaLes <valeria.l.1994@outlook.com>
2023-09-11 13:43:12 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryListEl = document.querySelector('.gallery')
+
+
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+
+   
+})
+.join("")
+
+
+
+galleryListEl.innerHTML = galleryItemsMarkup
+
+const simpleLightBox = new SimpleLightbox('.gallery__link', { captionsData: "alt", captionDelay: 250 });
