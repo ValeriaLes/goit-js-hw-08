9809f54efbf19388f891561faac364d583146ea3
ValeriaLes <valeria.l.1994@outlook.com>
2023-09-11 18:18:49 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+
+
+const iframe = document.querySelector('#vimeo-player');
+
+
+const player = new Player(iframe)
+
+player.on('timeupdate', throttle(timeUpDateFunc, 1000));
+
+function timeUpDateFunc (data) {
+    const currentTime = Math.round(data.seconds)
+    localStorage.setItem("videoplayer-current-time", currentTime)
+    console.log(currentTime)
+
+}
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+
+
+
