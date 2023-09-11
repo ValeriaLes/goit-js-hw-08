b310488b90573d91a9a2a5fb980b2ee929373d09
ValeriaLes <valeria.l.1994@outlook.com>
2023-09-11 23:44:52 +0200

    task2, task3 50%done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector('.feedback-form')
+const emailInput = document.querySelector('input[type="email"]')
+const messageInput = document.querySelector('textarea')
+const formData = {}
+
+populateInputArea()
+
+form.addEventListener('input', onFormInput)
+
+function onFormInput (event) {
+    formData[event.target.name] = event.target.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    
+}
+
+function populateInputArea () {
+    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"))
+    
+    if(savedFormData) {
+
+        emailInput.value = savedFormData.email;
+        messageInput.value = savedFormData.message
+        }
+}
+
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData)
+    event.target.reset()
+    localStorage.removeItem("feedback-form-state");
+
+}
